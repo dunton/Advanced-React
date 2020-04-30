fcd275dce792613ce4325878f695f0c923e1d43e
Ryan Dunton <ryan.dunton@equinox.com>
2020-04-30 10:56:36 -0400

    pagination

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,10 +3,12 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
+import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: id_DESC) {
       id
       title
       price
@@ -32,8 +34,13 @@ const ItemsList = styled.div`
 const Items = (props) => {
   return (
     <Center>
-      <p>items!</p>
-      <Query query={ALL_ITEMS_QUERY}>
+      <Pagination page={props.page} />
+      <Query
+        query={ALL_ITEMS_QUERY}
+        variables={{
+          skip: props.page * perPage - perPage,
+        }}
+      >
         {({ data, error, loading }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error: {error.message}</p>;
@@ -46,6 +53,7 @@ const Items = (props) => {
           );
         }}
       </Query>
+      <Pagination page={props.page} />
     </Center>
   );
 };
